e0210fe1206fdabcd28696d6c88771d4c148e330
Michele <michi.ele.busti@gmail.com>
2023-11-23 19:07:35 +0100

    Added individual .dat file for Si, Ge data

--- 1prova/caratteristicaGe.cpp
+++ 1prova/caratteristicaGe.cpp
@@ -4,17 +4,32 @@
 #include "TH1.h"
 #include "TStyle.h"
 
-void Car(Double_t Vmin, Double_t Vmax)
+#include <fstream>
+
+void carGe()
 {
     gStyle->SetOptFit(1111);
-    Int_t const n = 15;
-    Double_t V[n] = {44, 100, 130, 160, 180, 210, 240, 270, 290, 310, 340, 380, 400, 420, 440};                 // misure di ddp
-    Double_t I[n] = {0.01, 0.02, 0.05, 0.09, 0.14, 0.24, 0.4, 0.67, 0.92, 1.25, 1.96, 2.82, 3.74, 4.78, 6.03}; // misure di corrente
-    Double_t eV[n] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};                                              // errore su V
-    Double_t eI[n] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};                                              // errore su I
-    TCanvas *c = new TCanvas("c", "MyCanvas", 200, 200, 1000, 600);
-    TGraphErrors *g = new TGraphErrors(n, V, I, eV, eI);
-    TF1 *f = new TF1("f", "[0]*(exp(x/[1])-1)", Vmin, Vmax);
+    TGraphErrors *g = new TGraphErrors();
+    std::ifstream in1;
+    in1.open("diodoGe.dat");
+    double x, r, y, errx, erry;
+    int N;
+    while (1)
+    {
+        in1 >> x >> r >> y;
+        N = g->GetN();
+        errx = sqrt(2 * (r / 10) * (r / 10) + (x * 0.03) * (x * 0.03));
+        erry = y * 0.015;
+        if (!in1.good())
+        {
+            break;
+        }
+        g->SetPoint(N, x, y);
+
+        g->SetPointError(N, errx, erry);
+    }
+    in1.close();
+    TF1 *f = new TF1("f", "[0]*(exp(x/[1])-1)", 40, 450);
     f->SetParameters(0.01, 42); // I0 = uA per Ge, nA per Si
     // heta = 1 Ge, 2 Si; Vt = 26 mV a 300 K
     f->SetMarkerStyle(kOpenSquare);
--- 1prova/caratteristicaSi.cpp
+++ 1prova/caratteristicaSi.cpp
@@ -3,18 +3,33 @@
 #include "TGraphErrors.h"
 #include "TH1.h"
 #include "TStyle.h"
+#include <fstream>
+#include "TMath.h"
 
-void Car(Double_t Vmin, Double_t Vmax)
+void carSi()
 {
     gStyle->SetOptFit(1111);
-    Int_t const n = 15;
-    Double_t V[n] = {400, 460, 500, 520, 540, 560, 580, 600, 620, 640, 660, 680, 720, 760, 800};                 // misure di ddp
-    Double_t I[n] = {0.01, 0.02, 0.05, 0.08, 0.12, 0.18, 0.27, 0.43, 0.63, 0.98, 1.53, 1.67, 3.64, 7.89, 15.24}; // misure di corrente
-    Double_t eV[n] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};                                              // errore su V
-    Double_t eI[n] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};                                              // errore su I
-    TCanvas *c = new TCanvas("c", "MyCanvas", 200, 200, 1000, 600);
-    TGraphErrors *g = new TGraphErrors(n, V, I, eV, eI);
-    TF1 *f = new TF1("f", "[0]*(exp(x/[1])-1)", Vmin, Vmax);
+    TGraphErrors *g = new TGraphErrors();
+    std::ifstream in1;
+    in1.open("diodoSi.dat");
+    double x, r, y, errx, erry;
+    int N;
+    while (1)
+    {
+        in1 >> x >> r >> y;
+        N = g->GetN();
+        errx = 0; // sqrt(2 * (r / 10) * (r / 10) + (x * 0.03) * (x * 0.03));
+        erry = 0; // y * 0.015;
+        if (!in1.good())
+        {
+            break;
+        }
+        g->SetPoint(N, x, y);
+
+        g->SetPointError(N, errx, erry);
+    }
+    in1.close();
+    TF1 *f = new TF1("f", "[0]*(exp(x/[1])-1)", 350, 850);
     g->SetMarkerStyle(kOpenSquare);
     f->SetParameters(0.01, 42); // I0 = uA per Ge, nA per Si
     // heta = 1 Ge, 2 Si; Vt = 26 mV a 300 K

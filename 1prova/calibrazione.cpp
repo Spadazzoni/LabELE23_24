42cbb37ef3dbd23824abdb56b8760c1a1c38fa4f
Michele <michi.ele.busti@gmail.com>
2023-11-23 19:18:38 +0100

    Added data reading from file to calibrazione.cpp

--- 1prova/calibrazione.cpp
+++ 1prova/calibrazione.cpp
@@ -4,19 +4,34 @@
 #include "TGraphErrors.h"
 #include "TH1.h"
 #include "TStyle.h"
+#include <fstream>
 
-void Cal(Double_t Vmin, Double_t Vmax) {
+void cal()
+{
   gStyle->SetOptFit(1111);
-  Int_t const n = 9;
-  Double_t y[n] = {104.0, 200.0, 270.0, 360.0, 500.0,
-                   560.0 , 660.0, 760.0, 800.0};  // ddp misurata da oscilloscopio
-  Double_t x[n] = {100.0, 192.8, 262.1, 348.0, 483.4,
-                   542.1, 640.0, 730.0, 772.0};  // ddp misurata da multimetro
-  Double_t ex[n] = {0.4, 0.7, 0.9, 1, 1.6, 1.7, 2, 2.2, 2.4};
-  Double_t ey[n] = {4, 8, 10, 15, 18, 20, 22, 30, 31};
   TCanvas *c = new TCanvas("c", "MyCanvas", 200, 200, 1000, 600);
-  TGraphErrors *g = new TGraphErrors(n, x, y, ex, ey);
-  TF1 *f = new TF1("f", "[0]+[1]*x", Vmin, Vmax);
+  TGraphErrors *g = new TGraphErrors();
+  g->SetMarkerStyle(kOpenCircle);
+  std::ifstream in1;
+  in1.open("calibrazione.dat");
+  double x, r, y, errx, erry;
+  int N;
+  while (1)
+  {
+    in1 >> x >> r >> y;
+    N = g->GetN();
+    errx = 0; // sqrt(2 * (r / 10) * (r / 10) + (x * 0.03) * (x * 0.03));
+    erry = 0; // y * 0.015;
+    if (!in1.good())
+    {
+      break;
+    }
+    g->SetPoint(N, x, y);
+
+    g->SetPointError(N, errx, erry);
+  }
+  in1.close();
+  TF1 *f = new TF1("f", "[0]+[1]*x", 80, 820);
   f->SetParameters(0, 1);
   g->Fit(f, "QS0");
   g->SetTitle("Calibrazione dell'oscilloscopio");

0a5f7c174282e080e4cd0047a49b6ee0db3d73d2
Michele <michi.ele.busti@gmail.com>
2023-12-01 12:01:17 +0100

    Added data for 2prova

--- 2prova/fit.cpp
+++ 2prova/fit.cpp
@@ -7,7 +7,8 @@
 #include "TStyle.h"
 #include "TMultiGraph.h"
 
-void Fit(Double_t const Vmin, Double_t const Vmax) {
+void Fit(Double_t const Vmin, Double_t const Vmax)
+{
   gStyle->SetOptFit(1111);
   std::fstream in1;
   std::fstream in2;
@@ -18,33 +19,39 @@ void Fit(Double_t const Vmin, Double_t const Vmax) {
   TCanvas *c = new TCanvas();
   Double_t I;
   Double_t V;
-  Double_t fs;  // fondoscala
+  Double_t fs; // fondoscala
   Double_t ErrI;
   Double_t ErrV;
-  Int_t i = 0;  // indice file 1
-  Int_t j = 0;  // indice file 2
-  while (1) {
-    if (!in1.good()) {
+  Int_t i = 0; // indice file 1
+  Int_t j = 0; // indice file 2
+  while (1)
+  {
+    if (!in1.good())
+    {
       break;
     }
-    in1 >> V >> I >> fs;        // prima colonna V; seconda I; terza fs
-    Double_t ErrV1 = fs * 0.1;  // errore sullo 0 e sulla lettura
-    Double_t ErrV2 = V * 0.03;  // errore del costruttore
-    ErrV = sqrt(2 * ErrV1 * ErrV1 + ErrV2 * ErrV2);
+    in1 >> V >> I >> fs;       // prima colonna V; seconda I; terza fs
+    Double_t ErrV1 = fs * 0.1; // errore sulla lettura
+    Double_t Err0 = 5e-3 / 10;
+    Double_t ErrV2 = V * 0.03; // errore del costruttore
+    ErrV = sqrt(Err0 * Err0 + ErrV1 * ErrV1 + ErrV2 * ErrV2);
     ErrI = I * 0.03;
     g1->SetPoint(i, V, I);
     g1->SetPointError(i, ErrV, ErrI);
     ++i;
   }
   in1.close();
-  while (1) {
-    if (!in2.good()) {
+  while (1)
+  {
+    if (!in2.good())
+    {
       break;
     }
-    in2 >> V >> I >> fs;        // prima colonna V; seconda I; terza fs
-    Double_t ErrV1 = fs * 0.1;  // errore sullo 0 e sulla lettura
-    Double_t ErrV2 = V * 0.03;  // errore del costruttore
-    ErrV = sqrt(2 * ErrV1 * ErrV1 + ErrV2 * ErrV2);
+    in2 >> V >> I >> fs;       // prima colonna V; seconda I; terza fs
+    Double_t ErrV1 = fs * 0.1; // errore sulla lettura
+    Double_t Err0 = 5e-3 / 10;
+    Double_t ErrV2 = V * 0.03; // errore del costruttore
+    ErrV = sqrt(Err0 * Err0 + ErrV1 * ErrV1 + ErrV2 * ErrV2);
     ErrI = I * 0.03;
     g2->SetPoint(j, V, I);
     g2->SetPointError(j, ErrV, ErrI);
@@ -52,7 +59,9 @@ void Fit(Double_t const Vmin, Double_t const Vmax) {
   }
   in2.close();
   TF1 *f1 = new TF1("f1", "x/[0]-[1]", Vmin, Vmax);
+  f1->SetParameters(2, -20);
   TF1 *f2 = new TF1("f2", "x/[0]-[1]", Vmin, Vmax);
+  f2->SetParameters(2, -20);
   f1->SetLineColor(kBlue);
   f2->SetLineColor(kRed);
   g1->Fit(f1, "qs0");
@@ -64,4 +73,6 @@ void Fit(Double_t const Vmin, Double_t const Vmax) {
   mg->Add(g2);
   mg->SetTitle("Caratteristica I-V del transistor in regione attiva; V (udm); I (udm)");
   mg->Draw("ape");
+  f1->Draw("same");
+  f2->Draw("same");
 }
